056e0adb910300892f9635ac461fe665fea44472
nfapriyanto <nf.apriyanto@gmail.com>
2025-04-05 20:07:19 +0700

    done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function Heading() {
 function App() {
   return ( 
     <div className="App"> 
-      This is the starting code for "Your first component" ungraded lab 
       <Heading /> 
     </div> 
   ); 
